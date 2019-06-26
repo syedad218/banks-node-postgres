cc2dba2a27d5f4a866cd9438dfd0750b1cca0cf0
syedad218 <syed@cheenilabs.com>
2019-06-26 22:43:51 +0530

    database config env variables fix

---
+++ server/config/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  development: {
+    username: process.env.username,
+    password: null,
+    database: "bank_details",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: false
+  },
+  test: {
+    username: "root",
+    password: null,
+    database: "bank_details_test",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    operatorsAliases: false
+  },
+  production: {
+    username: process.env.username,
+    password: process.env.password,
+    database: process.env.database,
+    host: "ec2-54-204-35-248.compute-1.amazonaws.com",
+    dialect: "postgres",
+    operatorsAliases: false
+  }
+};
