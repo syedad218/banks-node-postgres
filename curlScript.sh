1f91e76b9fb9ae514a776bdb7d913cbf9f8f9b30
syedad218 <syed@cheenilabs.com>
2019-06-26 18:34:40 +0530

    @latest scripts

--- curlScript.sh
+++ curlScript.sh
@@ -1,13 +1,13 @@
 curl -X GET \
-  'http://127.0.0.1:8088/branch/MUMBAI/ABHYUDAYA%20COOPERATIVE%20BANK%20LIMITED?limit=5&offset=4' \
+  'http://gentle-dusk-26755.herokuapp.com/branch/MUMBAI/ABHYUDAYA%20COOPERATIVE%20BANK%20LIMITED?limit=5&offset=4' \
   -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoiZnlsZV9jb2RpbmdfY2hhbGxlbmdlIiwiaWF0IjoxNTYxNTQyODMzLCJleHAiOjE1NjE5NzQ4MzN9.s6-1of5-cSur6GDX5dqUguZBi3VlWOqyrPy-PoGKEW8' \
   -H 'Content-Type: application/json' \
-  -H 'Postman-Token: 86890fe9-6c9e-41ef-8000-4902edb3c198' \
+  -H 'Postman-Token: 31b14519-ac68-4624-b706-3548067ac6ee' \
   -H 'cache-control: no-cache'
 
 curl -X GET \
-  http://127.0.0.1:8088/branch/ABHY0065001 \
+  http://gentle-dusk-26755.herokuapp.com/branch/ABHY0065001 \
   -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoiZnlsZV9jb2RpbmdfY2hhbGxlbmdlIiwiaWF0IjoxNTYxNTQyODMzLCJleHAiOjE1NjE5NzQ4MzN9.s6-1of5-cSur6GDX5dqUguZBi3VlWOqyrPy-PoGKEW8' \
   -H 'Content-Type: application/json' \
-  -H 'Postman-Token: d335fbc1-15c4-4c3f-90b3-45bc3a2478f0' \
+  -H 'Postman-Token: a68c3d31-abd4-4dbf-9f80-87aa5b50bc92' \
   -H 'cache-control: no-cache'
