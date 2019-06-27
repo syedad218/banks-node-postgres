94b1c6b960e13070ab0ff2aae9734bb141761d8a
syedad218 <syed@cheenilabs.com>
2019-06-27 20:19:00 +0530

    updated curlScript.sh file

--- curlScript.sh
+++ curlScript.sh
@@ -1,9 +1,10 @@
 curl -X GET \
-  'http://gentle-dusk-26755.herokuapp.com/branch/MUMBAI/ABHYUDAYA%20COOPERATIVE%20BANK%20LIMITED?limit=5&offset=4' \
+  'http://gentle-dusk-26755.herokuapp.com/branch?city=MUMBAI&bank_name=ABHYUDAYA%20COOPERATIVE%20BANK%20LIMITED&limit=5&offset=4' \
   -H 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZXN0IjoiZnlsZV9jb2RpbmdfY2hhbGxlbmdlIiwiaWF0IjoxNTYxNTQyODMzLCJleHAiOjE1NjE5NzQ4MzN9.s6-1of5-cSur6GDX5dqUguZBi3VlWOqyrPy-PoGKEW8' \
   -H 'Content-Type: application/json' \
   -H 'Postman-Token: 31b14519-ac68-4624-b706-3548067ac6ee' \
   -H 'cache-control: no-cache'
+
 
 curl -X GET \
   http://gentle-dusk-26755.herokuapp.com/branch/ABHY0065001 \
